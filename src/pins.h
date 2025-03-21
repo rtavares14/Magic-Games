0a21e5703ed08af6a9e0a965c5dbd3bf3562cd9f
Rafael Tavares <554514@student.saxion.nl>
2025-03-21 12:21:08 +0100

    Pins and libraries

---
+++ src/pins.h
@@ -0,0 +1,21 @@
+#ifndef PINS_H
+#define PINS_H
+
+//potentiometer
+#define PIN_POT A0
+
+//buzzer and btn pull up
+#define PIN_BUZZER 2
+#define PIN_BUTTON 3
+
+//for LED light
+#define PIN_RED 4
+#define PIN_GREEN 5
+#define PIN_BLUE 6
+
+//for key and led 
+#define DIO_PIN 8  
+#define CLK_PIN 9  
+#define STB_PIN 10
+
+#endif
